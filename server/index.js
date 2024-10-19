62d031124724bbe8947e9c20b9f53020a14060d3
Dipak Shingade <dipakshingade342@gmail.com>
2024-10-19 23:00:56 +0530

    deep trying to deploy attemp 2

--- server/index.js
+++ server/index.js
@@ -8,37 +8,36 @@ import adminRoutes from "./routes/adminRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import facultyRoutes from "./routes/facultyRoutes.js";
 import { addDummyAdmin } from "./controller/adminController.js";
-const app = express();
-dotenv.config();
 
+async function startserver() {
+  try {
+    const app = express();
+    dotenv.config();
+    app.use(bodyParser.json({ limit: "30mb", extended: true }));
+    app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+    app.use(cors());
 
-async function startserver(){
-  try{
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+    app.use("/api/admin", adminRoutes);
+    app.use("/api/faculty", facultyRoutes);
+    app.use("/api/student", studentRoutes);
 
-app.use("/api/admin", adminRoutes);
-app.use("/api/faculty", facultyRoutes);
-app.use("/api/student", studentRoutes);
-
-const PORT = process.env.PORT || 5001;
-app.get("/", (req, res) => {
-  res.send("Hello to college erp API");
-});
-mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    addDummyAdmin();
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((error) => console.log("Mongo Error", error.message));
-}catch(e){
-  console.log(e);
-}
+    const PORT = process.env.PORT || 5001;
+    app.get("/", (req, res) => {
+      res.send("Hello to college erp API");
+    });
+    mongoose
+      .connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        addDummyAdmin();
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+      })
+      .catch((error) => console.log("Mongo Error", error.message));
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 startserver();
